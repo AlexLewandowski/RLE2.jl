3a36f2fd780b399627d6c5336656e640e6ecb81d
AlexLewandowski <lewandowskialexp@gmail.com>
2022-06-16 22:55:42 -0600

    update cedar

--- run_cc_gpu_cedar.sh
+++ run_cc_gpu_cedar.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=16                 # Number of nodes
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=16G
-#SBATCH --time=0-3:00
+#SBATCH --mem=24G
+#SBATCH --time=0-12:00
 module load julia/1.6.2 cuda cudnn
 
 julia experiment/run_parallel.jl --config_path $1 --num_workers 16
