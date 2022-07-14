de6adce2499170b93a01d692a59e10445cef86fe
AlexLewandowski <lewandowskialexp@gmail.com>
2022-07-14 16:56:50 -0600

    Load julia by default

--- run_cc_tasks.sh
+++ run_cc_tasks.sh
@@ -3,5 +3,6 @@
 #SBATCH --time=3:00:00
 #SBATCH --ntasks=20               # number of MPI processes
 #SBATCH --mem-per-cpu=12000M      # memory; default unit is megabytes
+module load julia/1.6.2
 
 julia experiment/run_parallel.jl --config_path $1 --num_workers 20
