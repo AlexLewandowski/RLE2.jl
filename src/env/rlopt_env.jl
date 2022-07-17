b98b6ec4f23e170ed08ed9f20797422db09395a9
AlexLewandowski <lewandowskialexp@gmail.com>
2022-07-17 16:45:45 -0600

    Uncomment exploration

--- src/env/rlopt_env.jl
+++ src/env/rlopt_env.jl
@@ -127,7 +127,7 @@ function reset!(env::AbstractRLOptEnv; saved_f = false, greedy = false)
     if saved_f == true && !isnothing(env.init_state[1])
         ps, re = Flux.destructure(env.init_state[1].f)
         if !greedy
-            # ps = [p .+ 0.01f0*randn(Float32, size(p)) for p in ps]
+            ps = [p .+ 0.01f0*randn(Float32, size(p)) for p in ps]
         end
         f = NeuralNetwork(re(ps))
         env.agent.subagents[1].model.f = f
